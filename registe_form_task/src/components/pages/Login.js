4cec3de037770e1af12506974e979a79c5ae16b3
yasaman <yasamanbarati81@gmail.com>
2022-05-09 01:04:35 +0430

    #connectApi  login page

--- registe_form_task/src/components/pages/Login.js
+++ registe_form_task/src/components/pages/Login.js
@@ -1,59 +1,60 @@
-import React, { Fragment , useState} from "react";
+import React, { Fragment, useState } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-export default class Login extends React.Component{
+// Data
+const data = [
+    {
+        email: 'admin@gmail.com',
+        password: '12345678'
+    },
+    {
+        email: 'admin1@gmail.com',
+        password: '88888888'
+    },
+]
 
-    constructor(props){
-        super(props);
-        this.state = {
-           email : '' ,
-           password : '',
-           redirectToReferrer : false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const Login = () => {
 
-    
 
-    handleChange(event){
-            this.setState({
-                [event.target.name] : event.target.value
-                
-            });
-    }
-    handleSubmit(event){
-            event.preventDefault();
+    // State 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    // const [data, setData] = useState([]);
 
-            const user = {
-            email : this.state.email,
-            password : this.state.password
-            };
-            
-      if(this.state.email && this.state.password)      
-        {
-     axios.post('http://localhost:5000/data',user)
-     .then((response) =>
-        { 
-            let userresponse = response;
-            console.log(userresponse.data);
-            if(userresponse){
-            sessionStorage.setItem('data',JSON.stringify(userresponse));
-            this.setState({redirectToReferrer: true});
-            alert("welcome");
+    // Send Data
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {
+            email: email,
+            password: password
+        }
+
+        console.log(user);
+
+        // axios.get('http://localhost:5000/data').then(result => {
+
+        //     setData(result.data.data);
+
+            if (email.trim().length === 0 || password.trim().length === 0) {
+                alert('ایمیل و پسورد را وارد کنید')
+            } else {
+                const result = data.some((item) => item.email === email && item.password === password)
+
+                if (result) {
+                    alert('خوش آمدید')
+                } else {
+                    alert('لطفا اطلاعات را بررسی کنید')
+                }
             }
-            alert("welcome");
-            
-        },this)
-        .catch((error) => alert(error))
-                   
+        // }).catch(err => {
+        //     console.log(err);
+        // })
     }
-}
-    render(){
+
     return (
-        
         <Fragment>
             <div className="container">
                 <div className="row justify-content-center">
@@ -65,14 +66,26 @@ export default class Login extends React.Component{
                             <form className="col-8">
                                 <div className="form-input">
                                     <span><i className="fa fa-envelope-o" /></span>
-                                    <input type="email" placeholder="Email Address" autoComplete="off" onChange={this.handleChange} tabIndex={10} required />
+                                    <input
+                                        type="email"
+                                        placeholder="Email Address"
+                                        autoComplete="off"
+                                        value={email}
+                                        onChange={(e) => { setEmail(e.target.value) }}
+                                        tabIndex={10}
+                                        required />
                                 </div>
                                 <div className="form-input">
                                     <span><i className="fa fa-key" /></span>
-                                    <input type="password" placeholder="Password" onChange={this.handleChange}required />
+                                    <input
+                                        type="password"
+                                        value={password}
+                                        placeholder="Password"
+                                        onChange={(e) => { setPassword(e.target.value) }}
+                                        required />
                                 </div>
                                 <div className="mb-2 d-flex justify-content-center">
-                                    <button type="submit" onChange={this.handleSubmit} className="btn btn-block px-4 py-2 mt-1">
+                                    <button type="submit" onClick={handleSubmit} className="btn btn-block px-4 py-2 mt-1">
                                         Login
                                     </button>
                                 </div>
@@ -87,8 +100,9 @@ export default class Login extends React.Component{
                         </div>
                     </div>
                 </div>
-            </div>
-        </Fragment>
-        )
-    }
+            </div >
+        </Fragment >
+    )
 }
+
+export default Login
